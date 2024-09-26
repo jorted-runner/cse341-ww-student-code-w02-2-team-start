ebc7362158383598980c38a48a72f16dc7a01e58
jorted-runner <jorted.runner1996@gmail.com>
2024-09-25 22:01:57 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const routes = require('express').Router();
+const temple = require('./temple');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+
+routes.use('/temples', temple);
+// routes.use(
+//   '/',
+//   (docData = (req, res) => {
+//     let docData = {
+//       documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
+//     };
+//     res.send(docData);
+//   })
+// );
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+module.exports = routes;
